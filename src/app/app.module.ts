508b3a4e082e6042bec26d6b0673c9b762820e23
Nikita Lutsai <pythonplant12@gmail.com>
2023-04-10 13:08:06 +0200

    prepare components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    BrowserModule,
+    PaisModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
